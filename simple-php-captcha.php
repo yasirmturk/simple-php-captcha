9ccb5e60a5f4f1532f1fc4ee25fcdfd9be880505
Cory LaViska <cory@abeautifulsite.net>
2015-08-21 20:09:54 -0400

    Fix for servers that use atomic deployments

--- simple-php-captcha.php
+++ simple-php-captcha.php
@@ -69,8 +69,14 @@ function simple_php_captcha($config = array()) {
     }
 
     // Generate HTML for image src
-    $image_src = substr(__FILE__, strlen( realpath($_SERVER['DOCUMENT_ROOT']) )) . '?_CAPTCHA&amp;t=' . urlencode(microtime());
-    $image_src = '/' . ltrim(preg_replace('/\\\\/', '/', $image_src), '/');
+    if ( strpos($_SERVER['SCRIPT_FILENAME'], $_SERVER['DOCUMENT_ROOT']) ) {
+        $image_src = substr(__FILE__, strlen( realpath($_SERVER['DOCUMENT_ROOT']) )) . '?_CAPTCHA&amp;t=' . urlencode(microtime());
+        $image_src = '/' . ltrim(preg_replace('/\\\\/', '/', $image_src), '/');
+    } else {
+        $_SERVER['WEB_ROOT'] = str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['SCRIPT_FILENAME']);
+        $image_src = substr(__FILE__, strlen( realpath($_SERVER['WEB_ROOT']) )) . '?_CAPTCHA&amp;t=' . urlencode(microtime());
+        $image_src = '/' . ltrim(preg_replace('/\\\\/', '/', $image_src), '/');
+    }
 
     $_SESSION['_CAPTCHA']['config'] = serialize($captcha_config);
 
